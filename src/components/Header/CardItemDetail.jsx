b376c7ffd4715dfa5423f42106bef576ec1fb921
Julian <gonzalez_jd@live.com.ar>
2023-11-14 01:50:09 -0300

    style

--- src/components/Header/CardItemDetail.jsx
+++ src/components/Header/CardItemDetail.jsx
@@ -5,6 +5,7 @@ import { collection, query, getDocs, where, documentId } from "firebase/firestor
 import { Card, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { CartContext } from "./CartContext";
+import "./CardItemDetail.css"
 
 
 
@@ -34,28 +35,35 @@ const CardItemDetail = () => {
     return (
       <div className="div-productos">
         <h1>Detalle: </h1>
-      <div className="text-center" >             
+      <div id="item" >             
         {products.map((category) => (
-          <Card key={category.id} category={category} className="card" style={{ maxWidth: '500px', height: "7500px" }}>
-            <Card.Body style={{ textAlign: 'center' }}>
-              <Card.Img variant="top" src={category.img} alt={category.title} width={"50%"} height={"45%"}/>
-              <Card.Title style={{ maxHeight: '3rem', overflow: 'hidden', textOverflow: 'ellipsis' }}>{category.title}</Card.Title>
-              <Card.Text style={{ maxHeight: '3rem', overflow: 'hidden', textOverflow: 'ellipsis' }}>{category.description}</Card.Text>
-              <Card.Text>Precio: ${category.price}</Card.Text>
-              <div className='mt-auto' >
+          <Card  key={category.id} category={category}  style={{ maxWidth: '800px', Width: '500px', minHeight: "700px" }}>
+            <Card.Body className="card-item" style={{ textAlign: 'center' }}>
+              <Card.Title >{category.title}
+              <Card.Img variant="top" src={category.img} alt={category.title} width={"50%"} height={"45%"}/></Card.Title>
+              <Card.Text style={{ fontSize:"30px"}}> Precio: ${category.price} </Card.Text>
+              <Card.Text style={{color:"red", fontSize:"20px"}}>Descripcion: <br /> {category.description}</Card.Text>
+              
+              <div className='mt-auto2' >
+             
               <Link to={`/detail/${category.id}`} key={category.id}>
-                <Button variant="danger" onClick={() => addToCart(category)}>
+                <Button style={{margin:"10px", justifyContenter:"center"}} variant="danger" onClick={() => addToCart(category)}>
                   Agregar al carrito
                 </Button>
-                <Button onClick={() => deleteProductById()}>Eliminar del carrito</Button>
-                <Button onClick={() => clearCart()}>Vaciar Carrito</Button>
-                <Link to="/checkout">
-                <Button variant="danger" onClick={() => addToCart(category)}>Finalizar Compra</Button>
-                <p>Total a Pagar: ${getTotalPrice()}</p>
-                </Link>
-
-
+                
+                <Button style={{margin:"10px", justifyContent:"center"}} onClick={() => deleteProductById()}>Eliminar del carrito</Button>
+                
+                <Button style={{margin:"10px", justifyContenter:"center"}} onClick={() => clearCart()}>Vaciar Carrito</Button>
               </Link>
+                
+                <Link to="/checkout">
+                <Button style={{margin:"10px", justifyContenter:"center"}} variant="danger" onClick={() => addToCart(category)}>Finalizar Compra</Button>
+                </Link>
+               <div className="total">
+                <p style={{marginTop:"20px", fontSize:"20px", justifyContent:"center"}}>Total a Pagar: ${getTotalPrice()}</p>
+               </div>
+
+
               </div>
   
             </Card.Body>
